4cf50c24998f5fb4aa8e77b589b9855c6f8bb6f1
Timofei Gladkikh <timofei_gladkikh@mail.ru>
2016-05-24 10:09:20 +0600

    LinesCounter

---
+++ Gladkikh.Nsudotnet.LinesCounter/Gladkikh.Nsudotnet.LinesCounter/LinesCounter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladkikh.Nsudotnet.LinesCounter
+{
+    class LinesCounter
+    {
+        private string extension;
+
+        public LinesCounter(string _extension)
+        {
+            extension = _extension;
+        }
+
+        public void CountLines()
+        {
+            var files = Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*." + extension, SearchOption.AllDirectories);
+            int amount = 0;
+            
+            foreach (var file in files)
+            {
+                using (var streamReader = new StreamReader(file))
+                {
+                    int count = 0;
+                    bool newLine = true;
+                    bool multiComment = false;
+                    bool comment = false;
+
+                    while (!streamReader.EndOfStream)
+                    {
+                        char symbol = (char)streamReader.Read();
+                        
+                        if (symbol == '\r' || symbol == ' ')
+                        {
+                            continue;
+                        }
+
+                        if (symbol == '/' && !comment)
+                        {
+                            symbol = (char)streamReader.Read();
+
+                            switch (symbol)
+                            {
+                                case '/':
+                                    comment = true;
+                                    continue;  
+                                case '*':
+                                    multiComment = true;
+                                    continue;         
+                            }
+                        }
+
+                        if (multiComment && symbol == '*')
+                        {
+                            symbol = (char)streamReader.Read();      
+                            
+                            if (symbol == '/')
+                            {
+                                multiComment = false;
+                                continue;
+                            }
+                        }
+                        
+                        if (symbol == '\n')
+                        {
+                            newLine = true;
+                            if (comment)
+                            {
+                                comment = false;
+                            }
+                            continue;
+                        }
+
+                        if (newLine && !comment && !multiComment)
+                        {
+                            count++;
+                            newLine = false;
+                        }
+                    }
+                    amount += count;
+                }
+            }
+            Console.WriteLine("Amount line in files with extension .{0}: {1}", extension, amount);
+        }
+    }
+}
---
+++ Gladkikh.Nsudotnet.LinesCounter/Gladkikh.Nsudotnet.LinesCounter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladkikh.Nsudotnet.LinesCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                var linesCounter = new LinesCounter(args[0]);
+                linesCounter.CountLines();
+                Console.ReadKey();
+            }
+            else 
+            {
+                Console.WriteLine("Type, please, extension of files in which you wanna count the number of rows");
+                Console.ReadKey();
+                return;
+            }
+        }
+    }
+}
